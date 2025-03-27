9f6756149637faef8f35b62546dfa251c6184dab
github-action-benchmark <github@users.noreply.github.com>
2025-03-27 20:34:22 +0000

    add availability-distribution-regression-bench (customSmallerIsBetter) benchmark result for 10d15dd1219b6124a265b8f9069916023e58b7ed

--- bench/availability-distribution-regression-bench/data.js
+++ bench/availability-distribution-regression-bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1743001726906,
+  "lastUpdate": 1743107661931,
   "repoUrl": "https://github.com/paritytech/polkadot-sdk",
   "entries": {
     "availability-distribution-regression-bench": [
@@ -61693,6 +61693,60 @@ window.BENCHMARK_DATA = {
           {
             "name": "test-environment",
             "value": 0.009017727866666752,
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "10196091+Ank4n@users.noreply.github.com",
+            "name": "Ankan",
+            "username": "Ank4n"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "10d15dd1219b6124a265b8f9069916023e58b7ed",
+          "message": "[AHM] Revert v17 migration of pallet-staking in Westend (#8059)\n\ncloses part of https://github.com/paritytech/polkadot-sdk/issues/8061.\n\n++ remove all epmb files.\n\nNote: Polkadot and Kusama are below this version (v15) and would not be\naffected.",
+          "timestamp": "2025-03-27T21:06:28+01:00",
+          "tree_id": "ba599faee07fbab292993b41aa8787d91b7bdc48",
+          "url": "https://github.com/paritytech/polkadot-sdk/commit/10d15dd1219b6124a265b8f9069916023e58b7ed"
+        },
+        "date": 1743107644900,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Sent to peers",
+            "value": 18481.666666666653,
+            "unit": "KiB"
+          },
+          {
+            "name": "Received from peers",
+            "value": 433.3333333333332,
+            "unit": "KiB"
+          },
+          {
+            "name": "availability-distribution",
+            "value": 0.012954427560000004,
+            "unit": "seconds"
+          },
+          {
+            "name": "test-environment",
+            "value": 0.008850425300000088,
+            "unit": "seconds"
+          },
+          {
+            "name": "availability-store",
+            "value": 0.15932469796000007,
+            "unit": "seconds"
+          },
+          {
+            "name": "bitfield-distribution",
+            "value": 0.026496444846666672,
             "unit": "seconds"
           }
         ]
